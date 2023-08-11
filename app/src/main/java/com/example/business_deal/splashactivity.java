ed83ca39bebf78c6bdd998ac243134603813dd96
Proloy51 <proloyphysics323@gmail.com>
2023-08-11 23:47:19 +0600

    Started with splash activity

---
+++ app/src/main/java/com/example/business_deal/splashactivity.java
@@ -0,0 +1,26 @@
+package com.example.business_deal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splashactivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1500);
+    }
+}
