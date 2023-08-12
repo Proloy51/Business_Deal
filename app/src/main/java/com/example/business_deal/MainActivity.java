d4b30477dd8d087ee20852255733fc5bd9b17d46
Proloy51 <proloyphysics323@gmail.com>
2023-08-12 17:32:25 +0600

    Sign up page completed

--- app/src/main/java/com/example/business_deal/MainActivity.java
+++ app/src/main/java/com/example/business_deal/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.business_deal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView signuptext,email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signuptext = findViewById(R.id.signupid);
+        email = findViewById(R.id.emailid);
+        password = findViewById(R.id.passwordid);
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              signuptext.setVisibility(View.GONE);
+            }
+        });
+
+
+
+        signuptext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
