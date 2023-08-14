991f3ce686a067d3728d74b47355ae78c1db267a
Proloy51 <proloyphysics323@gmail.com>
2023-08-14 12:47:43 +0600

    Data restoring completed

---
+++ app/src/main/java/com/example/business_deal/CustomAdapter.java
@@ -0,0 +1,65 @@
+package com.example.business_deal;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class CustomAdapter extends ArrayAdapter<Business_class> {
+
+    private Activity context;
+    private List<Business_class> business_order;
+
+    public CustomAdapter(Activity context, List<Business_class> business_order) {
+        super(context, R.layout.sample, business_order);
+        this.context = context;
+        this.business_order = business_order;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater layoutInflater = context.getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.sample,null,true);
+
+
+        Business_class business_class = business_order.get(position);
+
+        TextView nametext,fhnametext,adresstext,mobiletext,descriptiontext,totalweighttext,moneytext,tokentext,resertext,withawtext;
+        nametext = view.findViewById(R.id.nameid);
+        fhnametext = view.findViewById(R.id.fhnameid);
+        adresstext = view.findViewById(R.id.adressid);;
+        mobiletext = view.findViewById(R.id.mobileid);
+        descriptiontext = view.findViewById(R.id.descriptionid);
+        totalweighttext = view.findViewById(R.id.totalweightid);
+        moneytext = view.findViewById(R.id.moneyid);
+        tokentext = view.findViewById(R.id.tokenid);
+        resertext = view.findViewById(R.id.reserveid);
+        withawtext = view.findViewById(R.id.withdrawid);
+
+
+        nametext.setText(business_class.getName());
+        fhnametext.setText(business_class.getFhname());
+        adresstext.setText(business_class.getAdress());
+        mobiletext.setText(business_class.getMobno());
+        descriptiontext.setText(business_class.getDescription());
+        totalweighttext.setText(business_class.getTotalweight());
+        moneytext.setText(business_class.getMoney());
+        tokentext.setText(business_class.getTokenno());
+        resertext.setText(business_class.getDateofreservation());
+        withawtext.setText(business_class.getDateofwithdrawn());
+
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/business_deal/HomeActivity.java
+++ app/src/main/java/com/example/business_deal/HomeActivity.java
@@ -8,22 +8,41 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     private ImageButton addbutton;
     private FirebaseAuth mauth;
+    private ListView listView;
+    DatabaseReference databaseReference;
+    private List<Business_class> business_classList;
+    private CustomAdapter customAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         addbutton = findViewById(R.id.adddatabtn);
-
+        listView = findViewById(R.id.listviewid);
         mauth = FirebaseAuth.getInstance();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Record");
+
+        business_classList = new ArrayList<>();
+        customAdapter = new CustomAdapter(HomeActivity.this,business_classList);
 
         addbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,5 +71,50 @@ public class HomeActivity extends AppCompatActivity {
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onStart() {
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                business_classList.clear();
+                for(DataSnapshot dataSnapshot1 : snapshot.getChildren())
+                {
+                   Business_class business_class = dataSnapshot1.getValue(Business_class.class);
+                   business_classList.add(business_class);
+                }
+
+                listView.setAdapter(customAdapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        String name = String.valueOf(business_classList.get(position).getName());
+                        String fhname = String.valueOf(business_classList.get(position).getFhname());
+                        String adress = String.valueOf(business_classList.get(position).getAdress());
+                        String mobileno = String.valueOf(business_classList.get(position).getMobno());
+                        String description = String.valueOf(business_classList.get(position).getDescription());
+                        String totalweight = String.valueOf(business_classList.get(position).getTotalweight());
+                        String money = String.valueOf(business_classList.get(position).getMoney());
+                        String tokenno = String.valueOf(business_classList.get(position).getTokenno());
+                        String reservedate = String.valueOf(business_classList.get(position).getDateofreservation());
+                        String withdrawndate = String.valueOf(business_classList.get(position).getDateofwithdrawn());
+
+
+                        Intent intent = new Intent("name",name);
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        super.onStart();
     }
 }
